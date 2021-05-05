ff3d7d2e86b7ae3a51412cad429496cd3a6902b6
Ronnie740 <ronnieineza@gmail.com>
2021-05-05 16:03:02 +0300

    my project

---
+++ Internship_Assignment/src/layouts/MainLayout.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+      <div class="flex">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar id="logo">
+          
+          </q-avatar>
+          <q-avatar id="logo2">
+
+          </q-avatar>
+          Proom
+        </q-toolbar-title>
+      </q-toolbar>
+  
+      <q-tabs align="left">
+        <q-route-tab to="#" label="Courses" />
+        <q-route-tab to="#" label="Features" />
+        <q-route-tab to="#" label="Support" />
+      </q-tabs>
+      
+      <q-btn dense round flat icon="email">
+      <q-badge color="red" floating transparent>
+        3
+      </q-badge>
+    </q-btn>
+
+    <q-avatar>
+      <img src="/icons/pic_1.jpg" width="20px" height="20px">
+    </q-avatar>
+    </div>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
---
+++ Internship_Assignment/src/pages/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class=" flex-center">
+    <h3>Study different areas<br/> of web design easily<br/> in 3 Steps</h3>
+    <ol>
+      <li>Visit our website</li>
+      <li>Sign up</li>
+      <li>Find the course you want</li>
+    </ol>
+    <q-btn push color="accent" text-color="primary" label="Submit">
+    </q-btn>
+    <div class="color_block">
+
+  </div>
+  
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
+<style>
+
+</style>
